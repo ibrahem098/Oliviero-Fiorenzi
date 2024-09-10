da204b3b181b4aac7db7e12c514483396b41dc75
Ibrahim Ismail <ebrahemesmael@gmail.com>
2024-09-10 04:58:42 +0300

    added data ,functions,more.

---
+++ content.js
@@ -0,0 +1,195 @@
+let content = [{
+    id: 1,
+    title: 'MEMORY SPIN',
+    elemnts: [
+        `<p class="elemnt-title">MEMORY SPIN</p>`,
+        `<p class="elemnt-discription">Site specific installation at TURBO MILANO, 2022</p>`,
+        `<img src="/media/projects/MEMORY SPIN/1.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">MEMORY SPIN, 2022</p>`,
+        `<p class="elemnt-text">Through the power of play, Oliviero Fiorenzi constructs a complex figurative sign system thanks to which he enters a relationship with the context where he operates, producing pictorial, sculptural and site-specific installations.</p>`,
+        `<p class="elemnt-text">Discover the role of MEMORY SPIN in the entanglement between play and institution, in the essay “GIOCO E ISTITUZIONE – Una dissertazione breve” by Giacomo Paracciani.</p>`,
+        `<img src="/media/projects/MEMORY SPIN/2.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">MEMORY SPIN, 2022</p>`,
+        `<img src="/media/projects/MEMORY SPIN/3.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">MEMORY SPIN, 2022</p>`,
+        `<p class="elemnt-cridits">photo by</p> `,
+        `<p class="elemnt-cridits">@Piercarlo Quecchia - Dsl Studio</p> `,
+        `<p class="elemnt-cridits">All images copyright and courtesy of the artist</p> `
+    ]
+}, {
+    id: 2,
+    title: 'OTTO CIELI',
+    elemnts: [
+        `<p class="elemnt-title">OTTO CIELI</p>`,
+        `<p class="elemnt-discription">Mostra personale, galleria The Address, Brescia, 29.09 - 12.11 2022</p>`,
+        `<img src="/media/projects/OTTO CIELI/1.jpg" class="elemnt-image">`,
+        `<p class="elemnt-text">Otto Cieli, la mostra personale di Oliviero Fiorenzi sviluppata con la collaborazione dell’azienda tessile NT Majocchi. Negli spazi della galleria The Address l’artista presenta otto aquiloni pensati come soggetti e stati d’animo. Il Caso, la Lotta, il Gioco, la Passione, la Paura, lo Stupore, la Scoperta, la Contemplazione: ciascun aquilone rappresenta un capitolo dell’alfabeto simbolico di Fiorenzi e corrisponde a un paesaggio, a un cielo.</p>`,
+        `<img src="/media/projects/OTTO CIELI/2.jpg" class="elemnt-image">`,
+        `<p class="elemnt-text">Con Otto Cieli prosegue la ricerca di Fiorenzi che da anni lavora con l’imprevedibilità degli spazi e sperimenta l’opera come gioco, costruzione di esperienza e iscrizione di senso, agendo per ritrovare sempre lo stupore in ciò che si presenta aglI occhi. Per diversi mesi l’artista ha sperimentato le tecniche di realizzazione degli aquiloni e ne ha documentato il volO raccogliendo i materiali fotografici e audiovisivi che espone negli spazi di The Address.</p>`,
+        `<img src="/media/projects/OTTO CIELI/3.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">LA PAURA - La Fame d’Aria, 2022</p>`,
+        `<img src="/media/projects/OTTO CIELI/4.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">LA LOTTA - La Tigre, 2022</p>`,
+        `<img src="/media/projects/OTTO CIELI/5.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">LO STUPORE - Il Cavallo, 2022</p>`,
+        `<img src="/media/projects/OTTO CIELI/6.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">LA CONTEMPLAZIONE - Il Fuoco, 2022</p>`,
+        `<img src="/media/projects/OTTO CIELI/7.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">LA RICERCA - Le Mani, 2022</p>`,
+        `<img src="/media/projects/OTTO CIELI/8.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">LA PASSIONE - Il Fiore, 2022</p>`,
+        `<img src="/media/projects/OTTO CIELI/9.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">IL GIOCO - Il Cowboy, 2022</p>`,
+        `<img src="/media/projects/OTTO CIELI/10.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">THERE IS ALWAYS WIND, 2022</p>`,
+        `<img src="/media/projects/OTTO CIELI/11.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">THERE IS ALWAYS WIND, 2022</p>`,
+        `<img src="/media/projects/OTTO CIELI/12.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">THERE IS ALWAYS WIND, 2022</p>`,
+        `<img src="/media/projects/OTTO CIELI/13.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">THERE IS ALWAYS WIND, 2022</p>`,
+        `<p class="elemnt-cridits">For further information on the artworks, please contact the gallery: info@theaddressgallery.com</p>`,
+        `<p class="elemnt-cridits">All images copyright and courtesy of the artist</p>`,
+    ]
+}, {
+    id: 3,
+    title: 'MOVIN SIGN',
+    elemnts: [
+        `<p class="elemnt-title">MOVIN SIGN</p>`,
+        `<p class="elemnt-discription">MARZOCCARELAB - Residency program,</p>`,
+        `<p class="elemnt-discription">Demanio Marittimo km-278, 2022</p>`,
+        `<img src="/media/projects/MOVIN SIGN/1.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">MOVIN SIGN, Marzocca, 2022</p>`,
+        `<p class="elemnt-text">Proseguendo la ricerca SEGNALI, già presentata nel 2020 a Demanio Marittimo.KM- 278, Oliviero Fiorenzi elabora per Marzocca Re-lab un nuovo capitolo - “Moving Sign” che lo vede nuovamente in dialogo con il mare. Nel progetto SEGNALI, Fiorenzi, attraverso l’utilizzo del segnale marittimo come supporto e strumento, ha indagato il mare come spazio espositivo inedito, con l’oggetto che agisce come display sul quale riportare il suo personale codice segnico.</p>`,
+        `<img src="/media/projects/MOVIN SIGN/2.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">MOVIN SIGN, Marzocca, 2022</p>`,
+        `<p class="elemnt-text">In MOVING SIGN, l’oggetto, a supporto del codice visivo non è più una boa, ma una piccola imbarcazione con una vela dipinta, con la deriva che si trasforma in segnale mobile in mare guidato dallo stesso artista. La vela sarà dipinta con un disegno optical che cita il “Dazzle Camouflage”, un genere di patterns che veniva dipinto sulle navi da guerra della Marina Militare Inglese e Americana durante la Prima Guerra Mondiale; lo scopo non era quello di nascondere, ma bensì di attirare e confondere l’osservatore.</p>`,
+        `<img src="/media/projects/MOVIN SIGN/3.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">MOVIN SIGN, Marzocca, 2022</p>`,
+        `<p class="elemnt-text">L’artista con questo nuovo progetto si è posto lo stesso obiettivo: attirare lo sguardo delle persone insinuando in loro curiosità. Uscendo in barca per molti giorni consecutivi e navigando davanti al litorale di Marzocca, Oliviero Fiorenzi costruirà quindi una ruotine, e questo continuo veleggiare diventerà un nuovoelemento visivo in movimento inserito nel paesaggio.</p>`,
+        `<img src="/media/projects/MOVIN SIGN/4.jpg" class="elemnt-image">`,
+        `<p class="elemnt-image-comment">MOVIN SIGN, Marzocca, 2022</p>`,
+        `<p class="elemnt-text">Con questa pratica l’artista si trasforma in segnale, diventa oggetto osservato da chi frequenta la spiaggia, ma allo stesso tempo sarà un osservatore della scena. Nel susseguirsi dei giorni raccoglierà impressioni inedite, e ritornato a riva restituirà quanto vissuto in forma di racconto fotografico / pittorico. Così Marzocca diventa il playground in un gioco di sguardi e punti di vista.</p>`,
+        `<img src="/media/projects/MOVIN SIGN/5.jpeg" class="elemnt-image">`,
+        `<p class="elemnt-cridits">Photo by</p> `,
+        `<p class="elemnt-cridits">@Matteo Natalucci</p> `,
+        `<p class="elemnt-cridits">Images copyright and courtesy of the artist</p> `
+    ]
+}, {
+    id: 99,
+    title: 'CV',
+    elemnts: [
+        `
+        <img src="media/man.jpg">
+        <span class="name">Oliviero Fiorenzi</span>
+        <p class="bio">(Osimo, 1992), works between Milan and Ancona. Living between these two contexts, he developed a particular sensitivity for the theme of the landscape. Trough his personal experience, he constructs a complex figurative sign apparatus thanks to which he enters into a relationship with the context where he operates, producing pictorial, sculptural and site specific installations. <br> <br>
+            His works been exhibited in galleries, foundations and museums including: The Address in Brescia, Sonnenstube in Lugano, Feltrinelli Foundation, La Triennale in Milan, Ex-Dogana in Rome, La Mole in Ancona and Manifattura Tabacchi in Florence.
+        </p>
+        <div class="info">
+            <span>Upcoming</span>
+            <p>2023 - Site Specific project,  Spazio Fontanella, Rome <br>
+                2023 - Book Launch, “OTTO CIELI” published by Press Press<br>
+                2023 - Public Work, Tresoldi Academy #2 Sea, Ancona<br>
+            </p>
+            <span>Solo Show</span>
+            <p>2021 - Otto Cieli, Solo show, The Address Gallery, Brescia</p>
+            <span>Duo Show</span>
+            <p>2022 - AMORE 2000, with Edoardo Piermattei, Kunst Banhhoff, during Documenta 15, Kassel<br>
+                2020 - SSP, Oliviero Fiorenzi and Roberto Alfano, The Address Gallery, Brescia<br>
+                2019 - Crash Test, Oliviero Fiorenzi and Yoan Mudry, Sonnestube Offspace, Lugano<br>
+            </p>
+            <span>Group show</span>
+            <p>2022 - Poggio Tempesta, CF - Contemporary Fire, Cerreto Guidi<br>
+                2021 - When Urban Attitude become Contemporary Art, 21 Gallery, Villorba curated by Cesare Biasini Selvaggi<br>
+                2020 - Sleeping with the tiger, Group Show, K-Gold Gallery, Lesvos Greece<br>
+                2019 - Progresso Inconsapevole, Group Show, Fondazione Feltrinelli, Milano<br>
+                2018 - Turbosafary will never die but it did, Ex-Dogana, Roma Curated by Studio Volante, text by Vittorio Parisi<br>
+                2018 - Orogenesi, Studi Festival Milano, Studieo, Milano<br>
+                2018 - Il corso delle cose, Vaprio d’Adda, curated by Marco Scotini<br>
+                2018 - Avanzamento Progressivo, Milano curated by Altrove<br>
+                2017 -Ritual, Improper Walls Gallery, Wien<br>
+                2017 - The Great Learning, La Triennale Milano, curated by Marco Scotini<br>
+            </p>
+            <span>Public Art</span>
+            <p>2022 - TRAFFIC FESTIVAL, “Fiori della Pieve” Site-specific installation, San Lorenzo in Campo<br>
+                2022 - MEDA, Opera semi permantente, La Mole di Ancona<br>
+                2021 - 420 AirMarks, (Opera permanente) Manifattura Tabacchi di Firenze<br>
+                2020 - Segnali, Video installation, Demanio Marittimo Km-278, Marzocca<br>
+                2019  - Marziani Project, Site Specific Installation, Rome<br>
+                2019 - Enel, E-Distribuzione per Giro d’Italia with Turbosafary Collective, Pedaso<br>
+                2017 - Altrove Festival, Quinta Edizione, Catanzaro<br>
+                2016 - Floating Flags, curated by Gram Publishing, Venezia<br>
+                2015 - Pop Up Festival con FS Gruppo Ferrovie dello Stato Italiano, with Turbosafary Collective, Castelplanio<br>
+            </p>
+            <span>Residencies</span>
+            <p>2022 - Marzocca Relab, Demanio Marittimo km-278<br>
+                2021 - SUPERBLAST, Manifattura Tabacchi di Firenze<br>
+                2020 - Open Signs, Site-specific installation, La Mole di Ancona<br>
+                2019 - ResidenzaLaFornace, Spino d’Adda Curated By Olivotto and Manzoni<br>
+                2018 - Residenza Artistica “Shot- Inneschi contemporanei in polveriera” Garbagnate<br>
+            </p>
+            <span>Selected Publication</span>
+            <p>2021 - SUPERBLAST, published by Not | NERO on Theory<br>
+                2021 - When Urban Attitude become Contemporary Art, published by Silvana Editore<br>
+            </p>
+            <span>Selected Interviews</span>
+            <p>2022 - INSIDERART<br>
+                2022 - FORME UNICHE<br>
+                2020 - METAL MAGAZINE<br>
+            </p>
+            <span>Other projects</span>
+            <p>2016 -  “Una Losca Congiura ovvero Barbariccia contro Bonaventura” Scenography for Teatro dell’Arte della Triennale, Milano
+            </p>
+            <span>Studies</span>
+            <p>2019 MA, Visual Art and Curatorial Studies, NABA Milano<br>
+                2015 BA, Graphic Design and Art Direction, NABA, Milano<br>
+                2014 Erasmus, Willem de Kooning Academie, Rotterdam<br>
+                2010 Higher Diploma, Istituto Statale d’Arte Edgardo Mannucci, Ancona<br>
+            </p>`
+    ]
+}, {
+    id: 0,
+    title: 'clear',
+    elemnts: []
+}];
+
+/* 
+<p class="elemnt-title"></p>
+<p class="elemnt-discription"></p>
+<img src="" class="elemnt-image">
+<p class="elemnt-image-comment"></p>
+<p class="elemnt-text"></p>
+<p class="elemnt-cridits"></p> 
+*/
+
+let matchingContent;
+
+function getContent(ContentId) {
+    content.forEach((contentPage) => {
+        if (contentPage.id === ContentId) {
+            matchingContent = contentPage
+        }
+    })
+};
+
+document.querySelectorAll('.work-line').forEach((button) => {
+
+    button.addEventListener('click', () => {
+        document.querySelectorAll('.work-line').forEach((button) => {
+            button.style.color = 'rgb(0, 0, 0)';
+        });
+        let contentId = Number(button.dataset.selectid);
+        getContent(contentId);
+        if (contentId === 0) {
+            button.style.color = 'rgb(0, 0, 0)';
+        } else {
+            button.style.color = 'rgb(255,74,0)';
+        }
+        let html = ``
+        matchingContent.elemnts.forEach((elemnt) => {
+            html += elemnt
+        });
+
+        document.querySelector('.container').innerHTML = html;
+    });
+});
